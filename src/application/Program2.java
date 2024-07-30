2b573e5700b864510473ebe2fdcc489f95918667
Wagner perfeito <wagnerperfeito@wpcoms.com.br>
2024-07-30 14:15:35 -0300

    Reutilizando a instanciação

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -10,19 +10,23 @@ import model.entities.Seller;
 public class Program2 {
 
 	public static void main(String[] args) {
-		
+		System.out.println("=== TEST 1 : new obj ====");
 		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		Seller seller1 = sellerDao.findById(3);
-		
 		System.out.println(obj);
+		
+		System.out.println("=== TEST 2 : new seller ====");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
+		
+		System.out.println("=== TEST 3 : new sellerDao ====");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(sellerDao);
+		
+		System.out.println("=== TEST 4 : seller findbyId ====");
+		Seller seller1 = sellerDao.findById(3);
 		System.out.println(seller1);
+		
+
 	}
 
 }
